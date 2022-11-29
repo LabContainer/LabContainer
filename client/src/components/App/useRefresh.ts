1a79eec0ce40fa97169924718ee03e800d0bb03b
Sowrov Talukder <42917737+talukder-sowrov@users.noreply.github.com>
2022-11-28 21:45:27 -0500

    Infinite refresh fix attempt wip (#32)
    * First attempt to fix infinite refresh
    
    * Improved code to work and solve infinite refresh issue.

--- client/src/components/App/useRefresh.ts
+++ client/src/components/App/useRefresh.ts
@@ -1,33 +1,20 @@
 import { useState, useEffect, useContext } from "react"
+import useAPI from "../../api"
 import { AuthServiceAPI } from "../../constants"
 import { AuthContext } from "./AuthContext"
 
-export default function useRefresh() {
+export default function useRefresh(refresh_toggle: boolean) {
     const { refresh_token, setToken } = useContext(AuthContext)
     const [refresh, setRefresh] = useState(() => () => { })
-    useEffect(() => {
-        async function refresh(base_url: string, refresh_token: string, setToken: (new_token: string) => void) {
-            const response = await fetch(`${base_url}/webapp/refresh`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${refresh_token}`
-                }
-            })
-            if (response.ok) {
-                const json = await response.json()
-                setToken(json.access_token)
-            } else {
-                if (Math.floor(response.status / 100) === 4) {
-                    // Logout
-                    setToken("")
-                }
-                return undefined
-            }
+    const {WebappApi} = useAPI()
+    WebappApi.httpRequest.config.TOKEN = refresh_token;
+    useEffect(()=>{
+        const run = async () => {
+            const tokens = await WebappApi.webappRefresh();
+            const token = tokens.access_token || ""
+            WebappApi.httpRequest.config.TOKEN = token;
+            setToken(token)
         }
-        const caller = () => {
-            refresh(AuthServiceAPI, refresh_token, setToken)
-        }
-        setRefresh(() => caller)
-    }, [refresh_token, setToken, setRefresh])
-    return refresh
+        run();
+    }, [refresh_toggle])
 }
